3c9f644679dfef7589b9fff1572bf43282361ec3
Macailh <salvadorenriquegb@gmail.com>
2023-07-17 21:06:02 -0600

    refactor: for enter the resolution

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from pytube import YouTube
 
 
-url = input("Enter video url: ")
-resolution = "720p"
+def download_video(url, resolution='720p', path='./'):
 
-path = "./"
-filename = "video.mp4"
+    filename = "video.mp4"
 
-video = YouTube(url)
-stream = video.streams.filter(res=resolution).first()
-stream.download(filename=filename)
+    video = YouTube(url)
+    stream = video.streams.filter(res=resolution).first()
+    stream.download(filename=filename, output_path=path)
+
+
+if __name__ == "__main__":
+    url = input("Ingresa la URL del video de YouTube que deseas descargar: ")
+    resolution = input(
+        "Ingresa la resolución deseada (ej. 720p, 1080p, etc.): ")
+
+    download_video(url, resolution)
